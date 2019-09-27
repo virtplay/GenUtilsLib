d36b8e848ea958c066df3294d8455feb23585ab9
Karthik <karthikb@moback.com>
2019-09-27 16:10:39 +0530

    Initial commit, project setup

---
+++ GenUtilsExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenUtilsExamples
+//
+//  Created by Karthik on 27/09/19.
+//  Copyright © 2019 Moback. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GenUtilsLib/GenUtilsLib.h
@@ -0,0 +1,19 @@
+//
+//  GenUtilsLib.h
+//  GenUtilsLib
+//
+//  Created by Karthik on 27/09/19.
+//  Copyright © 2019 Moback. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenUtilsLib.
+FOUNDATION_EXPORT double GenUtilsLibVersionNumber;
+
+//! Project version string for GenUtilsLib.
+FOUNDATION_EXPORT const unsigned char GenUtilsLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenUtilsLib/PublicHeader.h>
+
+
